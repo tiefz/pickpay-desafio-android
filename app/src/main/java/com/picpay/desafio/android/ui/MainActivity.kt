32294d647adf355abd48740d81fc1556ae8b3383
Daniel Tief <chinorex@gmail.com>
2022-09-26 16:38:35 -0300

    Implemented observable to handle progressbar

--- app/src/main/java/com/picpay/desafio/android/ui/MainActivity.kt
+++ app/src/main/java/com/picpay/desafio/android/ui/MainActivity.kt
@@ -38,6 +38,7 @@ class MainActivity : AppCompatActivity() {
             users.apply {
                 adapter.users = users
             }
+            viewModel.updateView()
         }
     }
 }
--- app/src/main/java/com/picpay/desafio/android/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/picpay/desafio/android/viewmodel/MainViewModel.kt
@@ -1,5 +1,7 @@
 package com.picpay.desafio.android.viewmodel
 
+import android.view.View
+import androidx.databinding.ObservableField
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.picpay.desafio.android.repository.PicPayRepository
@@ -10,12 +12,16 @@ import javax.inject.Inject
 @HiltViewModel
 class MainViewModel @Inject constructor(picPayRepository: PicPayRepository) : ViewModel() {
 
-
     init {
         viewModelScope.launch {
             picPayRepository.refreshUsers()
         }
     }
 
+    val progressVisibility: ObservableField<Int> = ObservableField(View.VISIBLE)
     val contactList = picPayRepository.users
+
+    fun updateView() {
+        progressVisibility.set(View.GONE)
+    }
 }
